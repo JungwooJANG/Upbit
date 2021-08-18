09d6efcada9613558270a5c5ec10440e6a38b48e
JungwooJANG <89082847+JungwooJANG@users.noreply.github.com>
2021-08-18 15:33:35 +0200

    Update autotradeslack1.py

--- autotradeslack1.py
+++ autotradeslack1.py
@@ -47,6 +47,8 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
+sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
