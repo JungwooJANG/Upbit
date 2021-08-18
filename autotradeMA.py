ce8e5bc1b36979a678a4e874ddb3205b0d43989d
JungwooJANG <89082847+JungwooJANG@users.noreply.github.com>
2021-08-18 16:56:56 +0200

    Update autotradeMA.py

--- autotradeMA.py
+++ autotradeMA.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "ot8vi4MoyFMQXx8f6loUp0ZpQyrrKqEuaNp4xTWk"
-secret = "Wmft8TWOHnirrADScw84VsYzBBjWQPb7ww8LqBIl"
+access = ""
+secret = ""
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
